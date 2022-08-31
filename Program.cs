8ee2fb8818629cb31a79843606e4c68034753202
10Strel <manowartenarrows@gmail.com>
2022-08-31 20:35:11 +0300

    Final commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 ﻿Console.WriteLine("Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).\r\n");
+
+Console.WriteLine("Введите целое число:");
+
+bool res = int.TryParse(Console.ReadLine(), out int number);
+if (!res)
+{
+    Console.WriteLine("Ошибка ввода.\r\nВыполнение программы будет остановлено.");
+    return;
+}
+
+Console.WriteLine($"Результат: {number} {(number % 2 == 0 ? "четное" : "нечетное")} число.");
